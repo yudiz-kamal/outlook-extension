8fa20990a5b8deadc5dd9d67aed873cd1a2b26b0
yudiz-kamal <kamal.ansari@yudiz.com>
2026-02-02 18:35:05 +0530

    Add console logging for Office context in showAlert function for improved debugging

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -7,6 +7,7 @@ Office.onReady(() => {
 async function showAlert(event) {
   try {
     console.log("Button clicked! Calling POST to fakestoreapi...");
+    console.log("Office:", Office.context.mailbox.item);
 
     const apiEndpoint = "https://fakestoreapi.com/products";
     const response = await fetch(apiEndpoint, {
